4f25d97d1be2345e13a8c1bf3cf3d6a0f8e8d81e
Yahddyyp <Yahddyyp@gmail.com>
2026-07-18 19:57:02 +0530

    Simplify caveats: zsh/fish/nu examples + generic note for other shells

--- Formula/pass-tomb.rb
+++ Formula/pass-tomb.rb
@@ -27,33 +27,21 @@ class PassTomb < Formula
     <<~EOS
       Add to your shell config:
 
-      bash/zsh (~/.bashrc or ~/.zshrc):
+      zsh (~/.zshrc):
         export PASSWORD_STORE_ENABLE_EXTENSIONS=true
-        export PASSWORD_STORE_EXTENSIONS_DIR="#{opt_share}/pass-extensions"
+        export PASSWORD_STORE_EXTENSIONS_DIR="$HOME/.local/share/pass-extensions"
 
       fish (~/.config/fish/config.fish):
         set -x PASSWORD_STORE_ENABLE_EXTENSIONS true
-        set -x PASSWORD_STORE_EXTENSIONS_DIR "#{opt_share}/pass-extensions"
+        set -x PASSWORD_STORE_EXTENSIONS_DIR "$HOME/.local/share/pass-extensions"
 
       nushell (~/.config/nushell/env.nu):
         $env.PASSWORD_STORE_ENABLE_EXTENSIONS = "true"
-        $env.PASSWORD_STORE_EXTENSIONS_DIR = "#{opt_share}/pass-extensions"
+        $env.PASSWORD_STORE_EXTENSIONS_DIR = "$HOME/.local/share/pass-extensions"
 
-      elvish (~/.elvish/rc.elv):
-        set-env PASSWORD_STORE_ENABLE_EXTENSIONS true
-        set-env PASSWORD_STORE_EXTENSIONS_DIR #{opt_share}/pass-extensions
-
-      xonsh (~/.xonshrc):
-        $PASSWORD_STORE_ENABLE_EXTENSIONS = "true"
-        $PASSWORD_STORE_EXTENSIONS_DIR = "#{opt_share}/pass-extensions"
-
-      tcsh (~/.tcshrc):
-        setenv PASSWORD_STORE_ENABLE_EXTENSIONS true
-        setenv PASSWORD_STORE_EXTENSIONS_DIR #{opt_share}/pass-extensions
-
-      oil (~/.oilrc):
-        export PASSWORD_STORE_ENABLE_EXTENSIONS=true
-        export PASSWORD_STORE_EXTENSIONS_DIR="#{opt_share}/pass-extensions"
+      For other shells, set these environment variables however your shell prefers:
+        PASSWORD_STORE_ENABLE_EXTENSIONS=true
+        PASSWORD_STORE_EXTENSIONS_DIR="$HOME/.local/share/pass-extensions"
     EOS
   end
 end
